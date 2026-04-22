75eaef2bb7329c126b1c9782f24454cb3fef3712
mostafa el bakhti <mustmustapha67@gmail.com>
2026-04-22 18:33:50 +0000

    push

---
+++ src/__main__.py
@@ -0,0 +1,10 @@
+import argparse
+
+def parse_args():
+    args = argparse.ArgumentParser(
+        description="prompt to jsong "
+    )
+
+def main():
+    print("starting...")
+    args = parse_args()

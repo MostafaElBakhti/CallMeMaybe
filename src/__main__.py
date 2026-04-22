7e24d99394d32b022f41bf43fc25a8b8df31b2cf
mostafa el bakhti <mustmustapha67@gmail.com>
2026-04-22 18:44:01 +0000

    push

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,20 @@
 import argparse
 
 def parse_args():
-    args = argparse.ArgumentParser(
+    parse = argparse.ArgumentParser(
         description="prompt to jsong "
+    )
+
+    parse.add_argument(
+        "--input",
+        type=str,
+        default="data/input/function_calling_tests.jsong"
+    )
+
+    parse.add_argument(
+        "--function_definitions",
+        type=str,
+        default="data/input/function_definition.jsong"
     )
 
 def main():
